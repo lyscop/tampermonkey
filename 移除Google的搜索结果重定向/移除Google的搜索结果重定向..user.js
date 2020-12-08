d436ad6d76b401a3a63c6b2fe12cbfd11c7729bf
lyscop <63564691+lyscop@users.noreply.github.com>
2020-12-08 22:05:42 +0800

    Create 移除Google的搜索结果重定向..user.js

---
+++ 移除Google的搜索结果重定向/移除Google的搜索结果重定向..user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           移除Google的搜索结果重定向，包括其图片搜索
+// @namespace      google.com.xinggsf
+// @description    Remove all link redirection on Google Search Results,and Google image Search!
+// @homepageURL    https://greasyfork.org/scripts/19713
+// @include        https://www.google.*
+// @include        https://prism-kangaroo.glitch.me/search?*
+// @grant          none
+// @version        2019.11.11
+// ==/UserScript==
+
+main.addEventListener('mousedown', ev => {//图片搜索
+	if (ev.target.closest('a[jsaction^="mousedown:irc."] > img')) ev.stopPropagation();
+});
+Object.defineProperty(window, 'rwt', { writable: false });
