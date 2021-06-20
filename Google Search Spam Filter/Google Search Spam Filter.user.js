f0c20debf43a7e0b71dd5d5b43c6f197edc4773d
lyscop <63564691+lyscop@users.noreply.github.com>
2021-06-20 12:41:23 +0800

    Create Google Search Spam Filter.user.js

---
+++ Google Search Spam Filter/Google Search Spam Filter.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name            Google Search Spam Filter
+// @name:ru         Фильтр спама в результатах поиска Google
+// @description     removes blacklisted search results
+// @description:ru  скрывает ненужные результаты в выдаче
+// @namespace       https://www.google.com/search
+// @include         /^https?://(www\.)?google\.\w+//
+// @copyright       2018, StSav012
+// @author          StSav012
+// @grant           none
+// @version         0.1.20210411.2
+// @run-at          document-end
+// ==/UserScript==
+ 
+/* jshint esversion: 6 */
+/* jshint browser: true */
+ 
+"use strict";
+ 
+// blacklisted URLs
+var spamURLs = ['http://qaru.site/', 'https://stackoverrun.com/', 'https://manjaro.ru/', 'https://qastack.ru/', 'https://coderoad.ru/',
+                'https://overcoder.net/', 'https://overcoder.net:8443/', 'https://www.coder.work/', 'https://helpexe.ru', 'https://www.dvhu.com/',
+                'http://doc.crossplatform.ru/', 'https://quares.ru/', 'https://www.cnpython.com/', 'http://hk.uwenku.com/', 'https://www.debugcn.com/',
+                'https://tr.coredump.biz/', 'https://www.thinbug.com/', 'https://cloud6.net/'];
+ 
+for (let u of spamURLs) {
+  var junk = document.querySelectorAll('div a[href^="' + u + '"]:not([class])');
+  for (let j of junk) {
+    var p = j.parentNode;
+    while (p.tagName == 'DIV' && !p.classList.contains('g')) {
+      p = p.parentNode;
+    }
+    // console.log('found', p);
+    p.parentNode.removeChild(p);
+  }
+}
